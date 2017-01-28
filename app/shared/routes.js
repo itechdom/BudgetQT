26b4257cb78fb8263b6087f3b0e3e0c803d905cc
Osama Alghanmi <osamaalghanmi@Osamas-Mac-mini.local>
2017-01-28 13:02:43 -0700

    added routes

--- app/shared/routes.js
+++ app/shared/routes.js
@@ -2,3 +2,22 @@ import Home from '../views/home/home.js';
 import DailyBudget from '../views/daily-budget/daily-budget.js'; 
 import Friends from '../views/friends/friends.js'; 
 import Progress from '../views/progress/progress.js'; 
+
+export default {
+    home: {
+        title: 'Home',
+        Page: Home,
+    },
+    dailyBudget: {
+        title: 'Daily Budget',
+        Page: DailyBudget,
+    },
+    friends: {
+        title: 'Friends',
+        Page: Friends,
+    },
+    progress: {
+        title: 'Progress',
+        Page: Progress,
+    }
+}
