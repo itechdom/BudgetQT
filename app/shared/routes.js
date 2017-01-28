a7eb6aa06d836d10b434e15e529b762aed8efe93
Osama Alghanmi <osamaalghanmi@Osamas-Mac-mini.local>
2017-01-28 12:46:27 -0700

    changed folder structure

---
+++ app/shared/routes.js
@@ -0,0 +1,4 @@
+import Home from '../views/home/home.js'; 
+import DailyBudget from '../views/daily-budget/daily-budget.js'; 
+import Friends from '../views/friends/friends.js'; 
+import Progress from '../views/progress/progress.js'; 
