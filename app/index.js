57ef7e34a660d343ca3e5503ac1e0854cdfbaf90
Osama Alghanmi <osamaalghanmi@Osamas-Mac-mini.local>
2017-01-28 13:33:17 -0700

    completed a cycle

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import React, { Component } from 'react';
 
-import App from './App';
+import App from './app';
 
 
 export default function index() {
--- index.ios.js
+++ index.ios.js
@@ -1,26 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import native from './src';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class BudgetQT extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-      </View>
-    );
-  }
-}
-
-
-
-AppRegistry.registerComponent('BudgetQT', () => BudgetQT);
+native();
