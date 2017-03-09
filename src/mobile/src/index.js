91f9345519fc378a3d69fce2b628d323e1f084b4
Sam Alghanmi <oalghnmi@gmail.com>
2017-03-09 16:58:57 +0000

    cleaning up

--- src/mobile/src/index.js
+++ src/mobile/src/index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import React, { Component } from 'react';
 
-import App from './App/App.react';
+import App from './App/App.js';
 
 
 export default function index() {
